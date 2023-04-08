21acab2cb20bc5a59e2e79ac35ed0e9c9b842bc0
Woon Jung Koh <jjkoh21@gmail.com>
2023-04-08 16:50:08 +0900

    1234

--- src/je/panse/doro/GDSEMR_frame.java
+++ src/je/panse/doro/GDSEMR_frame.java
@@ -1,55 +1,58 @@
 package je.panse.doro;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import je.panse.doro.listner.TextAreaChangeListener;
 
+import je.panse.doro.listner.BlendColors;
+import je.panse.doro.listner.ButtonPanel;
+import je.panse.doro.listner.TextAreaChangeListener;
+	
 public class GDSEMR_frame extends JFrame {
 
-    public GDSEMR_frame() {
+    public GDSEMR_frame() throws Exception {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("My Frame");
-        setSize(1600, 1200);
+        setSize(1100, 1000/12*9);
 
         // Create West panel with tempOutputArea
         JTextArea tempOutputArea = new JTextArea();
-        tempOutputArea.setPreferredSize(new Dimension(400, 200));
+        tempOutputArea.setPreferredSize(new Dimension(350, 300));
         add(new JScrollPane(tempOutputArea), BorderLayout.WEST);
 
         // Create Center panel with 9 text areas
         JPanel centerPanel = new JPanel(new GridLayout(5, 2));
         JTextArea[] textAreas = new JTextArea[10];
-        String[] titles = { "CC>", "PI>", "ROS>", "PMH>", "S>", "O>", "Physical Exam","A>", "P>", "Co>" };
-        for (int i = 0; i < textAreas.length; i++) {
-            textAreas[i] = new JTextArea();
-            String inputData = titles[i];
-            textAreas[i].setText(inputData);
-            centerPanel.add(textAreas[i]);
-
-            JTextArea textArea = textAreas[i];
-            TextAreaChangeListener listener = new TextAreaChangeListener(textArea, tempOutputArea);
-            textArea.getDocument().addDocumentListener(listener);
-
-            // Set background color
-            float factor = (float) i / centerPanel.getComponentCount();
-            Color color = Color.getHSBColor(0.16f, factor, 3.0f);
-            textArea.setBackground(color);
-        }
-
-        // Add scroll pane to center panel
-        JScrollPane scrollPane = new JScrollPane(centerPanel);
-        add(scrollPane, BorderLayout.CENTER);
-
-        setVisible(true);
+        String[] titles = { "CC>", "PI>", "ROS>", "PMH>", "S>", "O>", "Physical Exam>","A>", "P>", "Comment>" };
+		        for (int i = 0; i < textAreas.length; i++) {
+					textAreas[i] = new JTextArea();
+					String inputData = titles[i];
+					textAreas[i].setText(inputData);
+					centerPanel.add(textAreas[i]);
+		
+					JTextArea textArea = textAreas[i];
+					TextAreaChangeListener listener = new TextAreaChangeListener(textArea, tempOutputArea);
+					textArea.getDocument().addDocumentListener(listener);
+					// Create background colors
+					BlendColors.blendColors(textArea, i);
+		        }
+			// Create button panel
+			ButtonPanel buttonPanel = new ButtonPanel();
+			add(buttonPanel, BorderLayout.NORTH);
+			ButtonPanel buttonPanel2 = new ButtonPanel();
+			add(buttonPanel2, BorderLayout.SOUTH);
+			// Add scroll pane to center panel
+			JScrollPane scrollPane = new JScrollPane(centerPanel);
+			add(scrollPane, BorderLayout.CENTER);
+			setVisible(true);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         new GDSEMR_frame();
     }
 }
