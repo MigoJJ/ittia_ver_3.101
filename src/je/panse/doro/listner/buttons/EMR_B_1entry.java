1ffee2984880ca4dbc3ad6eb400b28f903ef17bd
Woon Jung Koh <jjkoh21@gmail.com>
2023-04-11 13:01:23 +0900

    1234

--- src/je/panse/doro/listner/buttons/EMR_B_1entry.java
+++ src/je/panse/doro/listner/buttons/EMR_B_1entry.java
@@ -1,8 +1,13 @@
 package je.panse.doro.listner.buttons;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
+import javax.swing.RowFilter.Entry;
+
 import je.panse.doro.GDSEMR_frame;
+import je.panse.doro.chartplate.EMR_Write_To_Chartplate;
 import je.panse.doro.entry.EntryDir;
 import je.panse.doro.entry.IttiaEntry;
 import je.panse.doro.listner.IndentedTextArea;
@@ -18,28 +23,37 @@ public class EMR_B_1entry extends GDSEMR_frame {
 
         switch (noButton) {
             case 1:
-                if (panelLocation.equals("north")) {
-                	
-                	
-                	
-
-                    // When the clear button is clicked, clear all the input text areas
-                    for (int i = 0; i < textAreas.length; i++) {
-                    	textAreas[i].setText("");
-        				String inputData = titles[i] + "\t";
-        				textAreas[i].setText(inputData);
-                    }
-                    tempOutputArea.setText("");
-                   System.out.println("clear~~~ this button");
-                }
+					if (panelLocation.equals("north")) {
+					    // When the clear button is clicked, clear all the input text areas
+					    for (int i = 0; i < textAreas.length; i++) {
+					    	textAreas[i].setText("");
+							String inputData = titles[i] + "\t";
+							textAreas[i].setText(inputData);
+					    }
+					tempOutputArea.setText("");
+					System.out.println("clear~~~ this button");
+					}
                 break;
                 
             case 2:
+            		EMR_Write_To_Chartplate.copyToClipboard(tempOutputArea);
+            		EMR_Write_To_Chartplate.callsaveTextToFile(tempOutputArea);
                 break;
 
             case 3:
-            	IttiaEntry.main(null);
-//        			System.exit(0);
+            	String filepath = EntryDir.homeDir + "/tripikata/rescue/backup";
+                try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+                    String line;
+                    StringBuilder fileContents = new StringBuilder();
+                    while ((line = reader.readLine()) != null) {
+                        fileContents.append(line);
+                        fileContents.append(System.lineSeparator());
+                    }
+                    ProcessBuilder pb = new ProcessBuilder("gedit", filepath);
+                    pb.start();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }  	
                 break;
             case 4:
                 if (panelLocation.equals("north")) {
