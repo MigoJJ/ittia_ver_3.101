978d41e67f6b35ab83b4767cff6a1b2aa19f8e9a
Woon Jung Koh <jjkoh21@gmail.com>
2023-04-25 13:54:57 +0900

    1

--- src/je/panse/doro/samsara/EMR_east_buttons_obj.java
+++ src/je/panse/doro/samsara/EMR_east_buttons_obj.java
@@ -29,12 +29,9 @@ public class EMR_east_buttons_obj extends JFrame implements ActionListener {
         // Create buttons and add to array list
         for (String buttonName : buttonNames) {
             JButton button = new JButton(buttonName);
-<<<<<<< HEAD
-            button.setFont(new Font("Arial", Font.BOLD, 13));
-=======
+
             button.setFont(new Font("Arial", Font.PLAIN, 16));
             button.addActionListener(this); // Add this as the action listener
->>>>>>> refs/remotes/origin/Home2304201
             buttons.add(button);
             add(button);
         }
