bd1f70e80243d107d896c3eebe6ff300e76295fe
woonjungkoh <woonjungkoh@w-machine>
2023-05-06 17:36:16 +0900

    setLayout(new BoxLayout(this,

--- src/je/panse/doro/soap/plan/IttiaGDSPlanPanel_3.java
+++ src/je/panse/doro/soap/plan/IttiaGDSPlanPanel_3.java
@@ -1,17 +1,31 @@
 package je.panse.doro.soap.plan;
 
-import javax.swing.*;
+import javax.swing.*;	
 import java.awt.*;
 
 public class IttiaGDSPlanPanel_3 extends JPanel {
 
 	public IttiaGDSPlanPanel_3(IttiaGDSPlan frame) {
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        for (int i = 1; i <= 5; i++) {
-            add(new JCheckBox("Check Box " + i));
-            add(new JRadioButton("Radio Button " + i));
-            add(new JComboBox(new String[]{"Option 1", "Option 2", "Option 3"}));
-        }
+	    setLayout(new GridLayout(12, 2)); // Set layout to a 2x2 grid
+//        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+	
+	    // Create 10 checkboxes
+	    for (int i = 1; i <= 10; i++) {
+	        add(new JCheckBox("Check Box " + i));
+	    }
+	
+	    // Create 5 radio buttons
+	    ButtonGroup radioGroup = new ButtonGroup(); // Create a group for the radio buttons
+	    for (int i = 1; i <= 6; i++) {
+	        JRadioButton radioButton = new JRadioButton("Radio Button " + i);
+	        radioGroup.add(radioButton); // Add the radio button to the group
+	        add(radioButton);
+	    }
+	
+	    // Create 5 combo boxes
+	    for (int i = 1; i <= 6; i++) {
+	        add(new JComboBox(new String[]{"Option 1", "Option 2", "Option 3"}));
+	    }
 	}
 }
-
