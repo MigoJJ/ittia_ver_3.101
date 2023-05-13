9847c9e50380b8c00090137c6a12e71a0ea7c83c
woonjungkoh <woonjungkoh@w-machine>
2023-05-13 23:59:38 +0900

    123

--- src/je/panse/doro/samsara/comm/OpenOneNotePage.java
+++ src/je/panse/doro/samsara/comm/OpenOneNotePage.java
@@ -7,7 +7,7 @@ import java.net.URI;
 public class OpenOneNotePage {
 
     public static void main(String[] args) {
-        String oneNotePageLink = "paste_the_link_to_your_page_here";
+        String oneNotePageLink = "https://onedrive.live.com/redir?resid=CB5E027006DF23C3%218672&page=Edit&wd=target%28DM.one%7C902c6b4a-2317-4bb2-8012-9427f919c699%2FRetinipathy%7C06e70f13-93a8-485b-b465-91b5483c1f34%2F%29&wdorigin=NavigationUrl";
         try {
             Desktop.getDesktop().browse(new URI(oneNotePageLink));
         } catch (Exception e) {
