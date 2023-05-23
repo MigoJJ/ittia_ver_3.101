261dd54c18b0243c425c2bdafa45235588cf8d5b
woonjungkoh <woonjungkoh@w-machine>
2023-05-23 21:51:59 +0900

    1234

--- src/je/panse/doro/samsara/EMR_OBJ_excute/EMR_TFT.java
+++ src/je/panse/doro/samsara/EMR_OBJ_excute/EMR_TFT.java
@@ -1,185 +1,203 @@
 package je.panse.doro.samsara.EMR_OBJ_excute;
 
-import java.awt.Dimension;	
+import java.awt.BorderLayout;	
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Arrays;
-import javax.swing.BoxLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 import je.panse.doro.GDSEMR_frame;
-import je.panse.doro.samsara.comm.String_ArrowChange;
 
-public class EMR_TFT implements ActionListener, KeyListener {
-	private JFrame frame = new JFrame("EMR TFT");
-	private JTextArea outputArea;
-	private JButton clearButton, saveButton, quitButton;
-	private String[] labels = {"T3 : ", "free-T4 : ", "TSH : ", "Anti-TSH-R-Ab : ","Anti-mic-Ab : ","Anti-Tg-Ab : "};
-	private JTextField[] inputFields = new JTextField[labels.length];
-	
-	public EMR_TFT() {
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setLocationRelativeTo(null); // center the frame on the screen
-	    frame.setSize(400, 300);
-
-	    JPanel inputPanel = new JPanel(new GridLayout(labels.length, 2));
-	    inputFields = new JTextField[labels.length];
-
-	    for (int i = 0; i < labels.length; i++) {
-	        JLabel label = new JLabel(labels[i]);
-	        label.setHorizontalAlignment(SwingConstants.RIGHT);
-	        inputPanel.add(label);
-
-	        inputFields[i] = new JTextField();
-	        inputFields[i].setHorizontalAlignment(JTextField.CENTER);
-	        inputFields[i].addActionListener(this);
-	        inputFields[i].addKeyListener(this);
-	        inputFields[i].setPreferredSize(new Dimension(120, 50));
-	        inputPanel.add(inputFields[i]);
-	    }
-
-	    JPanel buttonPanel = new JPanel(new GridLayout(1, 3));
-
-	    clearButton = new JButton("Clear");
-	    saveButton = new JButton("Save");
-	    quitButton = new JButton("Quit");
-
-	    JButton[] buttons = { clearButton, saveButton, quitButton };
-	    String[] buttonActions = { "Clear", "Save", "Quit" };
-
-	    for (int i = 0; i < buttons.length; i++) {
-	        buttons[i].addActionListener(this);
-	        buttonPanel.add(buttons[i]);
-	        buttons[i].setActionCommand(buttonActions[i]);
-	    }
-
-	    outputArea = new JTextArea(10, 30);
-	    outputArea.setEditable(false);
-	    JScrollPane scrollPane = new JScrollPane(outputArea);
-
-	    JPanel mainPanel = new JPanel();
-	    mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-	    mainPanel.add(inputPanel);
-	    mainPanel.add(buttonPanel);
-	    mainPanel.add(scrollPane);
-
-	    frame.getContentPane().add(mainPanel);
-	    frame.pack();
-	    frame.setVisible(true);
-	}
-
-
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == clearButton) {
-            for (JTextField inputField : inputFields) {
-                inputField.setText("");
-            }
-            outputArea.setText("");
-        } else if (e.getSource() == saveButton) {
-        	
-        } else if (e.getSource() == quitButton) {
-            frame.dispose();
-        } else {
-        }
-    }
-
-    public void keyPressed(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        Object source = e.getSource();
-
-        // Check if Enter key is pressed and the source is an input field
-        if (keyCode == KeyEvent.VK_ENTER && Arrays.asList(inputFields).contains(source)) {
-            JTextField inputField = (JTextField) source;
-            int inputIndex = Arrays.asList(inputFields).indexOf(inputField);
-            
-            // Check if the text field is not empty
-            if (!inputField.getText().isEmpty()) {
-                int nextIndex = inputIndex + 1;
-                if (nextIndex < inputFields.length) {
-                    inputFields[nextIndex].requestFocusInWindow();
-                } 
-            }
-            
-            if (inputIndex == 3 && inputField.getText().isEmpty()) {
-                // do something if input is empty for the 4th field
-                saveResults3();
-            } else {
-                saveResults6();
-                // do something else for other input fields
-            }
-        }
-    }
-
-
-    private void saveResults6() {
-        for (JTextField field : inputFields) {
-            if (field.getText().isEmpty()) {
-//                JOptionPane.showMessageDialog(null, "Please fill in all fields.", "Error", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-        }
-        
-        String T3 = inputFields[0].getText(); 
-        T3 = String_ArrowChange.compareOriginAndLrange(T3, 0.90, 2.5);
-        String free_T4 = inputFields[1].getText(); 
-        free_T4 = String_ArrowChange.compareOriginAndLrange(free_T4, 10.6, 19.4);
-        String TSH = inputFields[2].getText(); 
-        TSH = String_ArrowChange.compareOriginAndLrange(TSH, 0.25, 5.00);
-        String Ab_TSH = inputFields[3].getText(); 
-        Ab_TSH = String_ArrowChange.compareOriginAndLrange(Ab_TSH, 0.8);
-        String Ab_Tg = inputFields[4].getText(); 
-        Ab_Tg = String_ArrowChange.compareOriginAndLrange(Ab_Tg, 115);
-        String Ab_Mic = inputFields[5].getText(); 
-        Ab_Mic = String_ArrowChange.compareOriginAndLrange(Ab_Mic, 34);
-        
-        String outputText = String.format(
-          	  "\n   " + T3 + "\tT3 (ug/dL)\n" +
-              "\n   " + free_T4 +"\tfree-T4(ug/dL)\n" +
-              "\n   " + TSH + "\tTSH(mIU/ml)\n" +
-        	    "\t" + Ab_TSH + "\tAnti-TSH-R-Ab (IU/L <1.75)\n" +
-        	    "\t" + Ab_Tg + "\tAnti-Thyrogobulin Ab (IU/mL <115)\n"+
-        	    "\t" + Ab_Mic + "\tAnti-microsomal Ab (IU/mL <34)\n"
-                );
-		outputArea.setText(outputText);
-		GDSEMR_frame.setTextAreaText(5, outputText);
-    }   
-
-    private void saveResults3() {
-
-        String T3 = inputFields[0].getText(); 
-        T3 = String_ArrowChange.compareOriginAndLrange(T3, 1.23, 3.08);
-        String free_T4 = inputFields[1].getText(); 
-        free_T4 = String_ArrowChange.compareOriginAndLrange(free_T4, 10, 19);
-        String TSH = inputFields[2].getText(); 
-        TSH = String_ArrowChange.compareOriginAndLrange(TSH, 0.4, 4.0);
-    	
-        String outputText = String.format(
-            	  "\n   " + T3 + "\tT3 (ug/dL)\n" +
-                  "\n   " + free_T4 +"\tfree_T4(ug/dL)\n" +
-                  "\n   " + TSH + "\tTSH(mIU/ml)\n" 
-        );
-
-		outputArea.setText(outputText);
-		GDSEMR_frame.setTextAreaText(5, outputText);
-		frame.dispose();
-            }
-
+public class EMR_TFT extends JFrame {
+    private JTextField[] textFields;
+    private JTextArea inputTextArea;
+    private String[] labels = {
+            "T3 (pg/mL):",
+            "Free T3 (pg/mL):",
+            "Free T4 (ng/dL):",
+            "TSH (mIU/L):",
+            "Anti-TSH-Receptor Antibodies (IU/L):",
+            "Anti-Thyroglobulin Antibodies (IU/mL):",
+            "Anti-Microsomal Antibodies (IU/mL):"
+        };
     
-    public void keyReleased(KeyEvent e) {}
+    public EMR_TFT() {
+        // Set JFrame properties
+        setTitle("Input Form");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 600);
+        setLocationRelativeTo(null);
 
-    public void keyTyped(KeyEvent e) {}
+        // Create panel
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        textFields = new JTextField[labels.length];
+
+        // Create input panel to hold labels and text fields
+        JPanel inputPanel = new JPanel(new GridLayout(labels.length, 2, 10, 10));
+
+        for (int i = 0; i < labels.length; i++) {
+            JLabel label = new JLabel(labels[i]);
+            label.setHorizontalAlignment(SwingConstants.RIGHT);
+
+            JTextField textField = new JTextField();
+            textField.setName(labels[i]); // Set the name of the text field
+
+            textField.setPreferredSize(new Dimension(textField.getPreferredSize().width, 30));
+            textField.setHorizontalAlignment(SwingConstants.CENTER);
+
+            textField.addKeyListener(new KeyListener() {
+                @Override
+                public void keyTyped(KeyEvent e) {
+                }
+
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                        int currentIndex = getCurrentIndex(textField);
+                        if (currentIndex < textFields.length - 1) {
+                            textFields[currentIndex + 1].requestFocus();
+                        }
+                    }
+                }
+
+                @Override
+                public void keyReleased(KeyEvent e) {
+                }
+            });
+
+            inputPanel.add(label);
+            inputPanel.add(textField);
+
+            textFields[i] = textField;
+        }
+
+        // Add input panel to the main panel
+        panel.add(inputPanel, BorderLayout.NORTH);
+
+        // Create input text area
+        inputTextArea = new JTextArea();
+        inputTextArea.setEditable(false);
+        inputTextArea.setPreferredSize(new Dimension(150, 100));
+
+        // Create buttons panel
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+
+        // Create buttons
+        JButton clearButton = new JButton("Clear");
+        JButton saveButton = new JButton("Save");
+        JButton quitButton = new JButton("Quit");
+
+        // Add button actions
+        clearButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                clearFields();
+            }
+        });
+
+        saveButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                saveData();
+            }
+        });
+
+        quitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String output = inputTextArea.getText();
+                GDSEMR_frame.setTextAreaText(5, output);
+                dispose();
+            }
+        });
+
+
+        // Add buttons to the button panel
+        buttonPanel.add(clearButton);
+        buttonPanel.add(saveButton);
+        buttonPanel.add(quitButton);
+
+        // Add input panel and input text area to the main panel
+        panel.add(inputTextArea, BorderLayout.CENTER);
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+
+        // Add panel to the frame
+        add(panel);
+
+        // Display the frame
+        setVisible(true);
+    }
+
+    private int getCurrentIndex(JTextField textField) {
+        for (int i = 0; i < textFields.length; i++) {
+            if (textFields[i] == textField) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void clearFields() {
+        for (JTextField textField : textFields) {
+            textField.setText("");
+        }
+    }
+
+    private void saveData() {
+        double[] ranges = {
+            80, 200,   // T3 (pg/mL) range
+            2.3, 4.2,  // Free T3 (pg/mL) range
+            0.8, 1.8,  // Free T4 (ng/dL) range
+            0.4, 4.0,  // TSH (mIU/L) range
+            Double.NEGATIVE_INFINITY, 1.75,  // Anti-TSH-Receptor Antibodies (IU/L) range
+            Double.NEGATIVE_INFINITY, 115,   // Anti-Thyroglobulin Antibodies (IU/mL) range
+            Double.NEGATIVE_INFINITY, 34    // Anti-Microsomal Antibodies (IU/mL) range
+        };
+
+        StringBuilder outputText = new StringBuilder();
+        outputText.append("\n");
+        for (int i = 0; i < labels.length; i++) {
+            String label = labels[i];
+            double rangeMin = ranges[i * 2];
+            double rangeMax = ranges[i * 2 + 1];
+
+            String value = textFields[i].getText();
+
+            if (!value.isEmpty()) {
+                double numericValue = Double.parseDouble(value);
+
+                if (numericValue < rangeMin) {
+                    outputText.append("    [ ▼   ").append(value).append("   ]\t").append(label).append("\n");
+                } else if (numericValue > rangeMax) {
+                    outputText.append("    [ ▲   ").append(value).append("   ]\t").append(label).append("\n");
+                } else {
+                    outputText.append("    [ ▷   ").append(value).append("   ]\t").append(label).append("\n");
+                }
+            }
+
+        }
+
+        inputTextArea.setText(outputText.toString());
+    }
 
     public static void main(String[] args) {
-        new EMR_TFT();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new EMR_TFT();
+            }
+        });
     }
 }
