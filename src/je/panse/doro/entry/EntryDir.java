58d451c057b53290a3d34aaa5c4490e30e5563d4
Woon Jung Koh <jjkoh21@gmail.com>
2023-07-31 12:24:32 +0900

    1

--- src/je/panse/doro/entry/EntryDir.java
+++ src/je/panse/doro/entry/EntryDir.java
@@ -3,15 +3,10 @@ package je.panse.doro.entry;
 
 public class EntryDir {
 	public static String currentDir = System.getProperty("user.dir");
-	// ----- "/home/woon/git/ittia_Version_2.1"
-<<<<<<< HEAD
-//	public static String homeDir = currentDir + "/src/je/panse/doro";
-	public static String homeDir = currentDir + "/je/panse/doro";
 
-=======
 	public static String homeDir = currentDir + "/src/je/panse/doro";
 //	public static String homeDir = currentDir + "/je/panse/doro";
->>>>>>> refs/remotes/origin/feature
+
 	public static String backupDir = homeDir + "/tripikata/rescue";
 	public static void main(String[] args) {
 	    // Get the path to the current user's directory
