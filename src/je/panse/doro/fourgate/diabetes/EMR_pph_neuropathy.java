894164345bb41192f0f7491bc08b124fab20c230
woonjungkoh <woonjungkoh@w-machine>
2023-09-01 21:52:12 +0900

    1

--- src/je/panse/doro/fourgate/diabetes/EMR_pph_neuropathy.java
+++ src/je/panse/doro/fourgate/diabetes/EMR_pph_neuropathy.java
@@ -1,6 +1,6 @@
 package je.panse.doro.fourgate.diabetes;
 
-import java.awt.BorderLayout;
+import java.awt.BorderLayout;	
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.GridLayout;
