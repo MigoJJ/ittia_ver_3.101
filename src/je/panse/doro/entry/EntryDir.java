bd0698fcd8407bccde23983670b536d4b8abc6b4
jae <jae@jae-virtual-machine>
2023-09-28 08:18:31 +0900

    1

--- src/je/panse/doro/entry/EntryDir.java
+++ src/je/panse/doro/entry/EntryDir.java
@@ -3,17 +3,11 @@ package je.panse.doro.entry;
 
 public class EntryDir {
 	public static String currentDir = System.getProperty("user.dir");
-<<<<<<< HEAD
 
-	public static String homeDir = currentDir + "/src/je/panse/doro";
-//	public static String homeDir = currentDir + "/je/panse/doro";
-
-=======
 	// ----- "/home/woon/git/ittia_Version_2.1"
 //	public static String homeDir = currentDir + "/src/je/panse/doro";
 	public static String homeDir = currentDir + "/je/panse/doro";
-	
->>>>>>> refs/remotes/origin/feature
+
 	public static String backupDir = homeDir + "/tripikata/rescue";
 	public static void main(String[] args) {
 	    // Get the path to the current user's directory
