a1109c98613239d7d3d9d2af976c7eee55c420e5
jae <jae@jae-virtual-machine>
2023-10-05 09:40:12 +0900

    HbA1c

--- src/je/panse/doro/samsara/EMR_OBJ_excute/EMR_HbA1c.java
+++ src/je/panse/doro/samsara/EMR_OBJ_excute/EMR_HbA1c.java
@@ -1,6 +1,9 @@
 package je.panse.doro.samsara.EMR_OBJ_excute;
 
 import javax.swing.*;
+
+import je.panse.doro.GDSEMR_frame;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
@@ -76,32 +79,7 @@ public class EMR_HbA1c extends JFrame {
                 break;
         }
     }
-
-    private void setupListeners() {
-        for (int i = 0; i < textFields.length; i++) {
-            final int currentFieldIndex = i;
-            textFields[i].addActionListener(e -> handleTextFieldAction(currentFieldIndex));
-        }
-    }
-
-    private void handleTextFieldAction(int currentFieldIndex) {
-        String textFieldText = textFields[currentFieldIndex].getText();
-        textArea.append(textFieldText + "\n");
-        textFields[currentFieldIndex].setText("");
-
-        if (currentFieldIndex == 2) {
-            Timer timer = new Timer(3000, e -> {
-                save();
-                dispose();
-            });
-            timer.setRepeats(false); // ensure the timer only fires once
-            timer.start();
-        } else if (currentFieldIndex < textFields.length - 1) {
-            textFields[currentFieldIndex + 1].requestFocus();
-        }
-    }
-
-
+    
     private void clear() {
         textArea.setText("");
         for (JTextField textField : textFields) {
@@ -112,7 +90,74 @@ public class EMR_HbA1c extends JFrame {
     private void save() {
         // Implement the saving logic here.
     }
+    
+    private void setupListeners() {
+        for (int i = 0; i < textFields.length; i++) {
+            final int currentFieldIndex = i;
+            textFields[i].addActionListener(e -> handleTextFieldAction(currentFieldIndex));
+        }
+    }
 
+    private void handleTextFieldAction(int currentFieldIndex) {
+        String sampleTime = "FBS";
+        String textFieldText = textFields[currentFieldIndex].getText();
+
+        if (currentFieldIndex == 0) {
+            if (!textFieldText.equals("0")) {
+                sampleTime = "PP" + textFieldText;
+            }
+            textArea.append("   " + sampleTime);
+        } else if (currentFieldIndex == 1) {
+            textArea.append("  [    " + textFieldText + "   ] mg/dL");
+        } else if (currentFieldIndex == 2) {
+            textArea.append("   HbA1c       [    " + textFieldText + "   ] %\n");
+            double hba1cDouble = Double.parseDouble(textFieldText);
+            hba1cCalc(hba1cDouble);
+            Timer timer = new Timer(5000, e -> {
+                save();
+                dispose();
+            });
+            timer.setRepeats(false); // Ensure the timer only fires once
+            timer.start();
+        }
+
+        textFields[currentFieldIndex].setText("");
+
+        if (currentFieldIndex < textFields.length - 1) {
+            textFields[currentFieldIndex + 1].requestFocus();
+        }
+    }
+    // Calculate the HbA1c value and update the text area
+    private void hba1cCalc(double hba1c_perc) {
+        double ifcc_hba1c_mmolmol = (hba1c_perc - 2.15) * 10.929;
+        double eag_mgdl = (28.7 * hba1c_perc) - 46.7;
+        double eag_mmoll = eag_mgdl / 18.01559;
+
+        String hba1cP = String.format(
+        		"\n\tIFCC HbA1c: [  %.0f  ] mmol/mol"
+        		+ "\n\teAG: [  %.0f  ] mg/dL"
+        		+ "\n\teAG: [  %.2f  ] mmol/l\n",
+                ifcc_hba1c_mmolmol, eag_mgdl, eag_mmoll);
+        textArea.append(hba1cP);
+    }
+
+    public static void getGlucoseControlStatus(double HbA1c) {
+        String status;
+        if (HbA1c > 9.0) {
+            status = "Very poor";
+        } else if (HbA1c >= 8.5 && HbA1c <= 9.0) {
+            status = "Poor";
+        } else if (HbA1c >= 7.5 && HbA1c < 8.5) {
+            status = "Fair";
+        } else if (HbA1c >= 6.5 && HbA1c < 7.5) {
+            status = "Good";
+        } else {
+            status = "Excellent";
+        }
+        String message = String.format("\n...now [ %s ] controlled glucose status", status);
+        GDSEMR_frame.setTextAreaText(8, message);
+        System.out.println(message);
+    }
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             EMR_HbA1c emr = new EMR_HbA1c();
