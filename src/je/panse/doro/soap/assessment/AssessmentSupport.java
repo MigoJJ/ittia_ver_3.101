592f2973206ec89cd9530e3b6eef14185919d2e9
jae <jae@jae-virtual-machine>
2023-10-09 10:27:25 +0900

    1

--- src/je/panse/doro/soap/assessment/AssessmentSupport.java
+++ src/je/panse/doro/soap/assessment/AssessmentSupport.java
@@ -1,11 +1,22 @@
 package je.panse.doro.soap.assessment;
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 
 import je.panse.doro.GDSEMR_frame;
-
-import java.awt.event.*;
 
 public class AssessmentSupport {
     private static final int FRAME_WIDTH = 800;
@@ -23,6 +34,7 @@ public class AssessmentSupport {
         frame.add(createTextArea(), BorderLayout.NORTH);
         frame.add(createComboBoxPanel(), BorderLayout.CENTER);
         frame.add(createButtonPanel(), BorderLayout.SOUTH);
+        frame.add(createCheckBoxPanel(), BorderLayout.EAST); // Add the checkbox panel to the EAST
         frame.setVisible(true);
     }
 
@@ -34,7 +46,7 @@ public class AssessmentSupport {
     }
 
     private JScrollPane createTextArea() {
-        textArea = new JTextArea(5, 40);
+        textArea = new JTextArea(10, 40);
         return new JScrollPane(textArea);
     }
 
@@ -46,7 +58,7 @@ public class AssessmentSupport {
             JComboBox<String> comboBox = new JComboBox<>(comboBoxOption);
             comboBox.addItemListener(e -> {
                 if (e.getStateChange() == ItemEvent.SELECTED) {
-                    textArea.append(e.getItem().toString() + "\n");
+                    textArea.append("   #  " + e.getItem().toString() + "\n");
                 }
             });
             centerPanel.add(comboBox);
@@ -63,122 +75,258 @@ public class AssessmentSupport {
     private JPanel createButtonPanel() {
         JPanel southPanel = new JPanel();
 
-        JButton clearButton = new JButton("Clear");
-	        clearButton.addActionListener(e -> {
-	            for (Component innerComp : centerPanel.getComponents()) {
-	                if (innerComp instanceof JComboBox) {
-	                    ((JComboBox<?>) innerComp).setSelectedIndex(0);
-	                }
-	            }
-	        textArea.setText("");
-	    });
+        // Define button properties in a structured manner
+        String[] buttonLabels = {"Clear", "Save", "Quit"};
+        ActionListener[] actionListeners = {
+            this::clearAction, 
+            this::saveAction,
+            this::quitAction
+        };
 
-        JButton saveButton = new JButton("Save");
-        	saveButton.addActionListener(e -> {
-        		String assess = textArea.getText();
-        		GDSEMR_frame.setTextAreaText(7, "\n");
-        		GDSEMR_frame.setTextAreaText(7, assess);
-        });
-
-        JButton quitButton = new JButton("Quit");
-	        quitButton.addActionListener(e -> {
-	            JFrame frame = (JFrame) SwingUtilities.getAncestorOfClass(JFrame.class, (Component) e.getSource());
-	            frame.dispose();
-        });
-
-        southPanel.add(clearButton);
-        southPanel.add(saveButton);
-        southPanel.add(quitButton);
+        // Create buttons using iteration
+        for (int i = 0; i < buttonLabels.length; i++) {
+            JButton button = new JButton(buttonLabels[i]);
+            button.addActionListener(actionListeners[i]);
+            southPanel.add(button);
+        }
 
         return southPanel;
+    }
+    private void clearAction(ActionEvent e) {
+        for (Component innerComp : centerPanel.getComponents()) {
+            if (innerComp instanceof JComboBox) {
+                ((JComboBox<?>) innerComp).setSelectedIndex(0);
+            }
+        }
+        textArea.setText("");
+    }
+
+    private void saveAction(ActionEvent e) {
+        String assess = textArea.getText();
+        GDSEMR_frame.setTextAreaText(7, "\n");
+        GDSEMR_frame.setTextAreaText(7, assess);
+    }
+
+    private void quitAction(ActionEvent e) {
+        JFrame frame = (JFrame) SwingUtilities.getAncestorOfClass(JFrame.class, (Component) e.getSource());
+        frame.dispose();
+    }
+
+    private JPanel createCheckBoxPanel() {
+        JPanel eastPanel = new JPanel(new GridLayout(0, 1)); // Use GridLayout with 0 rows to make it dynamic
+
+        // Create checkboxes and add them to the panel
+        String[] checkboxLabels = {
+            "Diabetes Mellitus", "HTN", "Dyslipidemia",
+            "Cancer", "Operation", "Thyroid Disease",
+            "Asthma", "Tuberculosis", "Pneumonia",
+            "Chronic/Acute Hepatitis", "GERD", "Gout",
+            "Arthritis", "Hearing Loss", "CVA",
+            "Depression", "Cognitive Disorder", "Allergy",
+            "Food", "Injection", "Medication"
+        };
+
+        for (String label : checkboxLabels) {
+            JCheckBox checkBox = new JCheckBox(label);
+            checkBox.addItemListener(e -> {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    // Append the checkbox label to the textArea
+                    textArea.append("   #  " + label + "\n");
+                }
+            });
+            eastPanel.add(checkBox);
+        }
+
+        return eastPanel;
     }
 
     private String[][] getComboBoxOptions() {
         return new String[][]  {
-        			 {
-							 "  # diabetes mellitus",
-							 "  # Prediabetes",
-							 "  # Gestational Diabetes Mellitus",
-							 "  # Diabetes mellitus F/U",
+        			 {       "****  DM  ****",
+							 "diabetes mellitus",
+							 "Prediabetes",
+							 "Gestational Diabetes Mellitus",
+							 "Diabetes mellitus F/U",
 							 
-							 "  # DM without complications",
-							 "  # DM with Retinopathy",
-							 "  # DM with Retinopathy : Non-proliferative diabetic retinopathy",
-							 "  # DM with Retinopathy : Proliferative diabetic retinopathy",
-							 "  # DM with Retinopathy : Macular edema",
-							 "  # DM with Nephropathy",
-							 "  # DM with Nephropathy with microalbuminuria",
-							 "  # DM with Nephropathy with CRF",
-							 "  # DM with Peripheral Neuropathy",
-							 "  # DM with Autonomic Neuropathy",
+							 "DM without complications",
+							 "DM with Retinopathy",
+							 "DM with Retinopathy : Non-proliferative diabetic retinopathy",
+							 "DM with Retinopathy : Proliferative diabetic retinopathy",
+							 "DM with Retinopathy : Macular edema",
+							 "DM with Nephropathy",
+							 "DM with Nephropathy with microalbuminuria",
+							 "DM with Nephropathy with CRF",
+							 "DM with Peripheral Neuropathy",
+							 "DM with Autonomic Neuropathy",
+        	            },
+        	            {   "****  Thyroid  ****",
+        	            	"Hyperthyroidism",
+        	            	"Hyperthyroidism : Graves' disease",
+        	            	"Hyperthyroidism F/U",
+        	            	"Hyperthyroidism with Pregnancy [ ] weeks F/U",
+        	            	"Hypothyroidism",
+        	            	"Hypothyroidism : Hashimoto's thyroiditis",
+        	            	"Hypothyroidism F/U",
+        	            	"Hypothyroidism with Pregnancy [ ] weeks F/U",
+        	            	
+        	            	"Thyroid nodule",
+        	            	"Thyroid cyst",
+        	            	"Simple Goiter",
+        	            	"Subacute Thyroiditis",
+        	            	"c/w Chronic Thyroiditis on USG",
+        	            	"Non-Thyroidal Illness F/U",
+        	            	
+        	            	"Papillary Thyroid Cancer OP(+) with tHypothyroidism",
+        	            	"Papillary Thyroid Cancer OP(+) RAI Tx(+) with hypothyroidism",
         	            },
         	            {
-        	            	"  # Hyperthyroidism",
-        	            	"  # Hyperthyroidism : Graves' disease",
-        	            	"  # Hyperthyroidism F/U",
-        	            	"  # Hyperthyroidism with Pregnancy [ ] weeks F/U",
-        	            	"  # Hypothyroidism",
-        	            	"  # Hypothyroidism : Hashimoto's thyroiditis",
-        	            	"  # Hypothyroidism F/U",
-        	            	"  # Hypothyroidism with Pregnancy [ ] weeks F/U",
-        	            	
-        	            	"  # Thyroid nodule",
-        	            	"  # Thyroid cyst",
-        	            	"  # Simple Goiter",
-        	            	"  # Subacute Thyroiditis",
-        	            	"  # c/w Chronic Thyroiditis on USG",
-        	            	"  # Non-Thyroidal Illness F/U",
-        	            	
-        	            	"  # Papillary Thyroid Cancer OP(+) with tHypothyroidism",
-        	            	"  # Papillary Thyroid Cancer OP(+) RAI Tx(+) withypothyroidism",
+        	            	"****  GI  ****",
+        	            	"GERD",
+        	            	"Chronic Atrophic Gastritis",
+        	            	"Chronic Superficial Gastritis",
+        	            	"r/o Erosive Gastritis",
+        	            	"r/o Irritable Bowel Syndrome",
+        	            	"Severe Constipation",
+        	            	"s/p Gastric Polyp   GFS : ",
+        	            	"s/p Duodenal Polyp   GFS : ",
+        	            	"s/p Duodenal Ulcer   GFS : ",
+        	            	"s/p CLO[ + ]   GFS : ",
+        	            	"s/p Colonic Polyp  CFS : ",
+        	            	"s/p Colonic diverticulum   CFS : ",
         	            },
         	            {
-        	            	"  # GERD",
-        	            	"  # Chronic Atrophic Gastritis",
-        	            	"  # Chronic Superficial Gastritis",
-        	            	"  # r/o Erosive Gastritis",
-        	            	"  # r/o Irritable Bowel Syndrome",
-        	            	"  # Severe Constipation",
-        	            	"  # Colonic Polyp",
-        	            	"  # Colonic diverticulum",
-        	            	"  # Gastric Polyp",
-        	            	"  # Duodenal Polyp",
-        	            	"  # Duodenal Ulcer",
-        	            	"  # CLO[ + ]",
-        	            	
-        	            	"  # Gilbert's syndrome",
-        	            	"  # s/p Hepatitis A infection",
-        	            	"  # HBsAg(+) Carrier",
-        	            	"  # Hepatitis C virus (HCV) chronic infection",
-        	            	"  # HCV-Ab(Positive) --> PCR(Negative) confirmed",
-        	            	"  # Hepatic Hemangioma",
-        	            	"  # Hepatic Cyst",
-        	            	"  # Hepatic Nodule",
-        	            	"  # Hepatic higher echoic nodule",
-        	            	"  # Fatty Liver",
-        	            	"  # Mild Fatty Liver",
-        	            	"  # Moderate Fatty Liver",
-        	            	"  # Severe Fatty Liver",
-        	            	"  # GB polyp",
-        	            	"  # GB stone" , 
-        	    			        	            },
-        	            {
-        	            	"  # s/p Cholecystectomy d/t GB stone" ,         
-        	            	"  # s/p Appendectomy",         
-        	            	"  # s/p Colon cancer op(+)" ,        
-        	            	"  # s/p Gastric cancer cancer op(+)",        
-        	            	"  # s/p TAH : Total Abdominal Hysterectomy",        
-        	            	"  # s/p TAH without BSO",   
-        	            	"  # s/p Mymectomy",
-        	            	"  # s/p Prostate cancer operation(+)",        
-        	            	"  # s/p Cataract OP(+)",   
+        	            	"****  Liver  ****",
+        	               	"Gilbert's syndrome",
+        	            	"s/p Hepatitis A infection",
+        	            	"HBsAg(+) Carrier",
+        	            	"Hepatitis C virus (HCV) chronic infection",
+        	            	"HCV-Ab(Positive) --> PCR(Negative) confirmed",
+        	            	"Hepatic Hemangioma",
+        	            	"Hepatic Cyst",
+        	            	"Hepatic Nodule",
+        	            	"Hepatic higher echoic nodule",
+        	            	"Fatty Liver",
+        	            	"Mild Fatty Liver",
+        	            	"Moderate Fatty Liver",
+        	            	"Severe Fatty Liver",
+        	            	"GB polyp",
+        	            	"GB stone" , 
+        	    		},
 
-        	            	"  # s/p Papillary Thyroid Cancer OP(+) with Hypothyroidism",        
-        	            	"  # s/p Papillary Thyroid Cancer OP(+) RAI Tx(+) with Hypothyroidism",   
-        	            	"  # s/p Breast Cancer Operation",
-        	            	"  # s/p Breast Cancer Operation+ ChemoTx(+)",        
-        	            	"  # s/p Breast Cancer Operation + RT(+)",        
-        	            	"  # s/p Breast Cancer Operation + ChemoTx(+) + RT(+)" 
+        	            {
+        	            	"****  Cancer  ****",
+        	            	"Stomach Cancer (Gastric Cancer)",
+        	            	"Liver Cancer",
+        	            	"Lung Cancer",
+        	            	"Colorectal Cancer",
+        	            	"Prostate Cancer",
+        	            	"Thyroid Cancer",
+        	            	"    s/p Papillary Thyroid Cancer OP(+) with tHypothyroidism",
+        	            	"    s/p Papillary Thyroid Cancer OP(+) RAI Tx(+) with hypothyroidism",
+        	            	"Breast Cancer",
+        	            	"    s/p Breast Cancer Operation",
+        	            	"    s/p Breast Cancer Operation+ ChemoTx(+)",        
+        	            	"    s/p Breast Cancer Operation + RT(+)",        
+        	            	"    s/p Breast Cancer Operation + ChemoTx(+) + RT(+)", 
+        	            	"Esophageal Cancer",
+        	            	"Pancreatic Cancer",
+        	            	"Bladder Cancer",
+        	            	"Cervical Cancer",
+        	            	"Ovarian Cancer",
+        	            	"Non-Hodgkin Lymphoma",
+        	            	"Leukemia",
+        	            	"Oral Cancer",
+        	            },
+        	            {
+        	            	"****  Benign Operation  ****",
+        	            	"Laparoscopic cholecystectomy (d/t GB stone)",
+        	            	"Endoscopic mucosal resection (EMR) for colorectal polyps",
+        	            	"Laparoscopic appendectomy",
+        	            	"Laparoscopic myomectomy ",
+        	            	"Laparoscopic hysterectomy for benign gynecological conditions",
+        	            	"    s/p TAH with BSO", 
+        	            	"Cataract surgery",
+        	            	"Tonsillectomy",
+        	            	"Thyroidectomy (thyroid nodule)",
+        	            	"Adenoidectomy (d/t PTH)",
+        	            	"Carpal tunnel release surgery",
+        	            	"Trigger finger release surgery",
+        	            	"Hernia repair",
+        	            	"Hemorrhoidectomy ",
+        	            	"Mastectomy for benign breast disease",
+        	            	"Septoplasty (nasal septum correction surgery)",
+        	            },
+        	            {   "**** Pulmonary disease  ****",
+        	            	"Chronic obstructive pulmonary disease (COPD)",
+        	            	"Asthma",
+        	            	"    s/p Asthma currently no significant probelm",
+        	            	"Bronchitis",
+        	            	"Pneumonia",
+        	            	"Bronchiectasis",
+        	            	"s/p  Tuberculosis , pulmnary",
+        	            	"s/p  Pneumothorax",
+        	            	"s/p  Pleural effusion",
+        	            	"Interstitial lung disease",
+        	            	"Sleep apnea",
+        	            	"Pulmonary embolism",
+        	            	"Cystic fibrosis",
+        	            	"Occupational lung diseases",
+        	            	"Lung infections",
+        	            },
+        	            
+        	            {   "**** Cardiovascular disease  ****",
+        	            	"Hypertension ",
+        	            	"Aortic aneurysm",
+        	            	"Deep vein thrombosis (DVT) and pulmonary embolism (PE)",
+        	            	"    s/p Deep vein thrombosis (DVT)",
+        	            	"Coronary artery disease (CAD)",
+        	            	"   s/p Angina Pectoris",
+        	            	"   s/p Myocardiac Infaction",
+        	            	"   s/p Coronary Spasm",
+        	            	"Arrhythmia",
+        	            	"    s/p Atrial fibrillation (AFib)",
+        	            	"    s/p Atrial flutter",
+        	            	"    s/p Supraventricular tachycardia (SVT)",
+        	            	"    s/p Ventricular fibrillation (VFib)",
+        	            	"    s/p Sinus bradycardia",
+        	            	"    s/p Sinus tachycardia",
+        	            	"    s/p Sick sinus syndrome",
+        	            	"    s/p Long QT syndrome",
+        	            	"    s/p Brugada syndrome",
+        	            	"    s/p Wolff-Parkinson-White syndrome",
+        	            	"    s/p Catecholaminergic polymorphic ventricular tachycardia (CPVT)",
+        	            	"Heart failure",
+        	            	"Stroke",
+        	            	"Valvular heart disease",
+        	            	"Peripheral artery disease (PAD)",
+        	            	"Cardiomyopathy",
+
+        	            	"Congenital heart disease",
+        	            	"Endocarditis (inflammation of the inner lining of the heart)",
+        	            	"Pericarditis (inflammation of the lining around the heart)",
+        	            	"Myocarditis (inflammation of the heart muscle)",
+        	            	"Cardiovascular disease due to diabetes",
+        	            },
+        	            {   "**** CVA  ****",
+        	            	"s/p Stroke  CVA",
+        	            	"Dementia (Cognitive Disorder)",
+        	            	"Alzheimer's disease",
+        	            	"Parkinson's disease",
+        	            	"Cerebral palsy",
+        	            	".",
+        	            	"Migraine",
+        	            	"Brain tumor",
+        	            	"    s/p Brain tumor : mningioma",
+        	            	"Hydrocephalus",
+        	            	"Traumatic brain injury",
+        	            	".",
+        	            	"Epilepsy",
+        	            	"Multiple sclerosis",
+        	            	"Meningitis",
+        	            	"Encephalitis",
+        	            	"Hydrocephalus",
+        	            	"Huntington's disease",
+        	            	"Amyotrophic lateral sclerosis (ALS)",
         	            },
         	            // You can add more options arrays here if needed
         	        };        
