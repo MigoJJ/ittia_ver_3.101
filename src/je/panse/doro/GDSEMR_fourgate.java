7d0c89103f1882874909eb1d22b2e7b5eb98d7df
jae <jae@jae-virtual-machine>
2023-10-09 11:01:30 +0900

    1

--- src/je/panse/doro/GDSEMR_fourgate.java
+++ src/je/panse/doro/GDSEMR_fourgate.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import je.panse.doro.samsara.EMR_east_buttons_obj;
 import je.panse.doro.samsara.EMR_CCPIPMH.EMRPMH;
 import je.panse.doro.soap.assessment.AssessmentSupport;
+import je.panse.doro.soap.cc.CCSupport;
 import je.panse.doro.soap.pe.EMR_PE_general;
 import je.panse.doro.soap.plan.IttiaGDSPlan;
 import je.panse.doro.soap.ros.EMR_ROS;
@@ -21,6 +22,8 @@ public class GDSEMR_fourgate extends GDSEMR_frame {
 		System.out.println("Double-clicked on: text >>> " + text);
 		if (text.contains("PMH>")) {
 			EMRPMH.main(text);
+		} else if (text.contains("CC>")) {
+			CCSupport.main(null);
 		} else if (text.contains("ROS>")) {
 				EMR_ROS.main(null);
 		} else if (text.contains("S>")) {
