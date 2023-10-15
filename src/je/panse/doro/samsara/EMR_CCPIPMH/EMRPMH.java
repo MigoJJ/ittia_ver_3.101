5c30c2715ab395c878955d9effbf223d6541670b
jae <jae@jae-virtual-machine>
2023-10-15 11:28:38 +0900

    1

--- src/je/panse/doro/samsara/EMR_CCPIPMH/EMRPMH.java
+++ src/je/panse/doro/samsara/EMR_CCPIPMH/EMRPMH.java
@@ -11,7 +11,6 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
@@ -20,10 +19,9 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
-
 import je.panse.doro.GDSEMR_frame;
 
-public class EMRPMH<pmhxTextArea> extends JFrame implements ActionListener {
+public class EMRPMH extends JFrame implements ActionListener {
 
     private JTextArea pmhxTextArea, commentpmh;
     private ArrayList<JCheckBox> checkBoxList;
@@ -37,54 +35,81 @@ public class EMRPMH<pmhxTextArea> extends JFrame implements ActionListener {
 
         // Create a panel to hold the checkboxes
         Font font = new Font("DejaVu Sans", Font.PLAIN, 12);
-//        Font font = new Font("Arial", Font.PLAIN, 12);
         JPanel pmhxPanel = new JPanel();
-        pmhxPanel.setLayout(new GridLayout(0, 3)); // Set the layout to a grid with 3 columns
+        pmhxPanel.setLayout(new GridLayout(0, 3));
 
         // Create the checkboxes and add them to the panel
         checkBoxList = new ArrayList<JCheckBox>();
         String[] checkboxLabels = { "Diabetes Mellitus", "HTN", "Dyslipidemia\n", "Cancer", "Operation",
                 "Thyroid Disease\n", "Asthma", "Tuberculosis", "Pneumonia\n", "Chronic/Acute Hepatitis", "GERD",
-                "Gout\n", "Arthritis", "Hearing Loss", "CVA\n", "Depression", "Cognitive Disorder\n", "Allergy\n",
-                "Food", "Injection", "Medication\n" };
+                "Gout\n", "Arthritis", "Hearing Loss", "CVA\n", "Depression", "Cognitive Disorder\n", "...",
+                "Angina Pectoris", "AMI", "Arrhythmia\n", "Allergy\n", "...", "...", "Food", "Injection",
+                "Medication\n" };
 
-		// Create a scrollable text area to display the medical history
-		pmhxTextArea = new JTextArea();
-		pmhxTextArea.setPreferredSize(new Dimension(600, 200));
-		pmhxTextArea.setText(" -\n");
-		pmhxTextArea.setEditable(true);
-		pmhxTextArea.setFont(font);
+        JTextArea[] textAreas = createTextAreas(font);
 
-		// Create a scrollable text area to add comment
-		commentpmh = new JTextArea();
-		commentpmh.setPreferredSize(new Dimension(600, 200));
-		commentpmh.setEditable(true);
-		commentpmh.setFont(font);
-		
-		for (String label : checkboxLabels) {
-			JCheckBox checkbox = new JCheckBox(label);
-		    checkbox.setFont(font);
-		    checkBoxList.add(checkbox);
-		    pmhxPanel.add(checkbox);
+        for (String label : checkboxLabels) {
+            JCheckBox checkbox = createCheckBox(label, font);
+            checkBoxList.add(checkbox);
+            pmhxPanel.add(checkbox);
+            addActionListenerToCheckBox(checkbox, label);
+        }
 
-		    checkbox.addItemListener(new ItemListener() {
-		        public void itemStateChanged(ItemEvent e) {
-		            if (e.getStateChange() == ItemEvent.SELECTED) {
-		                // Checkbox is checked, add text to pmhxTextArea
-		                pmhxTextArea.append("         " +dsquare+ " " + label + "\n");
-		            } else {
-		                // Checkbox is unchecked, remove text from pmhxTextArea
-		                String text = pmhxTextArea.getText().replace(label + "\n", "");
-		                pmhxTextArea.setText(text);
-		            }
-		        }
-		    });
-		}
+        JScrollPane pmhxScrollPane = new JScrollPane(textAreas[0]);
+        JScrollPane commentpmhPane = new JScrollPane(textAreas[1]);
 
-        JScrollPane pmhxScrollPane = new JScrollPane(pmhxTextArea);
-        JScrollPane commentpmhPane = new JScrollPane(commentpmh);
+        JPanel buttonPanel = createButtonPanel();
 
-        // Create a panel to hold the buttons
+        add(pmhxPanel, BorderLayout.NORTH);
+        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, pmhxScrollPane, commentpmhPane);
+        add(splitPane, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
+
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private JTextArea[] createTextAreas(Font font) {
+        JTextArea[] textAreas = new JTextArea[2];
+
+        pmhxTextArea = new JTextArea();
+        pmhxTextArea.setPreferredSize(new Dimension(600, 200));
+        pmhxTextArea.setText(" -\n");
+        pmhxTextArea.setEditable(true);
+        pmhxTextArea.setFont(font);
+
+        commentpmh = new JTextArea();
+        commentpmh.setPreferredSize(new Dimension(600, 200));
+        commentpmh.setEditable(true);
+        commentpmh.setFont(font);
+
+        textAreas[0] = pmhxTextArea;
+        textAreas[1] = commentpmh;
+
+        return textAreas;
+    }
+
+    private JCheckBox createCheckBox(String label, Font font) {
+        JCheckBox checkbox = new JCheckBox(label);
+        checkbox.setFont(font);
+        return checkbox;
+    }
+
+    private void addActionListenerToCheckBox(JCheckBox checkbox, String label) {
+        checkbox.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    pmhxTextArea.append("         " + dsquare + " " + label + "\n");
+                } else {
+                    String text = pmhxTextArea.getText().replace(label + "\n", "");
+                    pmhxTextArea.setText(text);
+                }
+            }
+        });
+    }
+
+    private JPanel createButtonPanel() {
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout());
         JButton saveShowButton = new JButton("Save and Show");
@@ -94,21 +119,11 @@ public class EMRPMH<pmhxTextArea> extends JFrame implements ActionListener {
         buttonPanel.add(clearButton);
         buttonPanel.add(saveQuitButton);
 
-        // Add everything to the main frame
-        add(pmhxPanel, BorderLayout.NORTH);
-        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, pmhxScrollPane, commentpmhPane);
-        add(splitPane, BorderLayout.CENTER);
-        add(buttonPanel, BorderLayout.SOUTH);
-
-        // Register action listeners for the buttons
         saveShowButton.addActionListener(this);
         clearButton.addActionListener(this);
         saveQuitButton.addActionListener(this);
-        
-        // Display the frame
-        pack();
-        setLocationRelativeTo(null);
-        setVisible(true);
+
+        return buttonPanel;
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -158,9 +173,7 @@ public class EMRPMH<pmhxTextArea> extends JFrame implements ActionListener {
         }
     }
 
-
     public static void main(String text) throws IOException {
         EMRPMH gui = new EMRPMH();
-//        gui.actionPerformed(new ActionEvent(gui, 0, "Save and Show"));
     }
 }
