7132acaf67a2e650cadfa3f9af3844949af39bbb
migowj <migowj@migowj-2001831>
2023-11-05 20:15:56 +0900

    row cell

--- src/je/panse/doro/fourgate/emrgdsfujtable/EMRGDS_FU_Jtable.java
+++ src/je/panse/doro/fourgate/emrgdsfujtable/EMRGDS_FU_Jtable.java
@@ -1,16 +1,19 @@
 package je.panse.doro.fourgate.emrgdsfujtable;
 
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellEditor;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.Vector;
+import javax.swing.*;
+import javax.swing.table.*;
 
+import je.panse.doro.entry.EntryDir;
+
+// Main Class for creating the GUI Table
 public class EMRGDS_FU_Jtable {
+    
     private JFrame frame;
-    private JTable table;
+    public static JTable table;
     private DefaultTableModel model;
     private JButton saveButton, loadButton;
 
@@ -22,31 +25,39 @@ public class EMRGDS_FU_Jtable {
         frame.setVisible(true);
     }
 
+    // Initialize the main JFrame
     private void initializeFrame() {
         frame = new JFrame("EMRGDS_FU_Jtable");
         frame.setSize(1600, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    // Setup the table with the necessary properties and model
     private void initializeTable() {
-        model = new DefaultTableModel(10, 10);
+        String[] columnNames = {"Diabetes Mellitus", "Hypertension", "Hypercholesterolemia", "Thyroid", "URI",
+                                "Atypical chest pain", "Osteoporosis", "8", "9", "10"};
+        model = new DefaultTableModel(columnNames, 10);
         table = new JTable(model);
+        
+        for (int i = 0; i < table.getColumnCount(); i++) {
+            table.getColumnModel().getColumn(i).setCellRenderer(new MultiLineCellRenderer());
+        }
+        
         table.setDefaultEditor(Object.class, new MultiLineCellEditor());
         setRowAndColumnDimensions();
     }
 
+    // Set specific row and column dimensions
     private void setRowAndColumnDimensions() {
-        // Example: Set heights for the first three rows
         table.setRowHeight(0, 150);  
-        table.setRowHeight(1, 30);
-        table.setRowHeight(2, 40);
-
-        // Example: Set widths for the first three columns
+        table.setRowHeight(1, 130);
+        table.setRowHeight(2, 140);
         table.getColumnModel().getColumn(0).setPreferredWidth(100);
         table.getColumnModel().getColumn(1).setPreferredWidth(50);
         table.getColumnModel().getColumn(2).setPreferredWidth(75);
     }
 
+    // Initialize the save and load buttons with actions
     private void initializeButtons() {
         saveButton = new JButton("Save Data");
         saveButton.addActionListener(e -> saveData());
@@ -54,19 +65,20 @@ public class EMRGDS_FU_Jtable {
         loadButton.addActionListener(e -> loadData());
     }
 
+    // Layout components on the frame
     private void layoutComponents() {
         JScrollPane scrollPane = new JScrollPane(table);
         JPanel panel = new JPanel();
         panel.add(saveButton);
         panel.add(loadButton);
-
         frame.setLayout(new BorderLayout());
         frame.add(scrollPane, BorderLayout.CENTER);
         frame.add(panel, BorderLayout.SOUTH);
     }
 
+    // Serialize and save the table data
     private void saveData() {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("tabledata.ser"))) {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(EntryDir.homeDir + "/fourgate/emrgdsfujtable/tabledata.ser"))) {
             out.writeObject(model.getDataVector());
             JOptionPane.showMessageDialog(frame, "Data Saved Successfully!");
         } catch (IOException ex) {
@@ -74,31 +86,40 @@ public class EMRGDS_FU_Jtable {
         }
     }
 
+    // Load the serialized table data
     private void loadData() {
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("tabledata.ser"))) {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(EntryDir.homeDir + "/fourgate/emrgdsfujtable/tabledata.ser"))) {
             Vector<? extends Vector> data = (Vector<? extends Vector>) in.readObject();
             Vector<String> columnNames = new Vector<>();
+            
             for (int i = 0; i < model.getColumnCount(); i++) {
                 columnNames.add(model.getColumnName(i));
             }
+            
             model.setDataVector(data, columnNames);
             JOptionPane.showMessageDialog(frame, "Data Loaded Successfully!");
-
-            // Reapply the row and column dimensions after loading the data
             setRowAndColumnDimensions();
-
         } catch (IOException | ClassNotFoundException ex) {
             ex.printStackTrace();
         }
     }
 
+    // Print table data to console
+    private void printTableData() {
+        for (int row = 0; row < table.getRowCount(); row++) {
+            for (int col = 0; col < table.getColumnCount(); col++) {
+                System.out.print(table.getValueAt(row, col) + "\t");
+            }
+            System.out.println();  // New line after each row
+        }
+    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new EMRGDS_FU_Jtable());
     }
 }
 
-// MultiLineCellEditor class moved outside the main class
+// Cell editor that supports multi-line input
 class MultiLineCellEditor extends AbstractCellEditor implements TableCellEditor {
     private JScrollPane scrollPane;
     private JTextArea textArea;
@@ -108,6 +129,18 @@ class MultiLineCellEditor extends AbstractCellEditor implements TableCellEditor 
         textArea.setWrapStyleWord(true);
         textArea.setLineWrap(true);
         scrollPane = new JScrollPane(textArea);
+        textArea.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    int currentColumn = EMRGDS_FU_Jtable.table.getSelectedColumn();
+                    if (currentColumn < EMRGDS_FU_Jtable.table.getColumnCount() - 1) {
+                        EMRGDS_FU_Jtable.table.changeSelection(EMRGDS_FU_Jtable.table.getSelectedRow(), currentColumn + 1, false, false);
+                        e.consume();
+                    }
+                }
+            }
+        });
     }
 
     @Override
@@ -119,5 +152,34 @@ class MultiLineCellEditor extends AbstractCellEditor implements TableCellEditor 
     @Override
     public Object getCellEditorValue() {
         return textArea.getText();
+    }
+}
+
+// Cell renderer that displays multi-line content
+class MultiLineCellRenderer extends JTextArea implements TableCellRenderer {
+    
+    public MultiLineCellRenderer() {
+        setWrapStyleWord(true);
+        setLineWrap(true);
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        setText((value != null) ? value.toString() : "");
+        
+        if (isSelected) {
+            setBackground(table.getSelectionBackground());
+            setForeground(table.getSelectionForeground());
+        } else {
+            setBackground(table.getBackground());
+            setForeground(table.getForeground());
+        }
+        
+        setSize(table.getColumnModel().getColumn(column).getWidth(), getPreferredSize().height);
+        if (table.getRowHeight(row) != getPreferredSize().height) {
+            table.setRowHeight(row, getPreferredSize().height);
+        }
+        
+        return this;
     }
 }
