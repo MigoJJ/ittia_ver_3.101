25e118d5b09c1feb2f0b5eb52e9d5093fcef3579
migowj <migowj@migowj-2001831>
2024-01-07 20:48:11 +0900

    123

--- src/je/panse/doro/fourgate/routinecheck/routinecheck.java
+++ src/je/panse/doro/fourgate/routinecheck/routinecheck.java
@@ -7,14 +7,14 @@ public class routinecheck {
     
     public static void GDSRC(String[] args) {
     	String cdate = Date_current.main("d");
-            GDSEMR_frame.setTextAreaText(0, "GDS clinic Routine check");
+            GDSEMR_frame.setTextAreaText(0, "\tGDS clinic Routine check");
             GDSEMR_frame.setTextAreaText(7, "\n  #  GDS clinic Routine check  [ " + cdate + " ]");
 
     }
     public static void HCRC(String[] args) {
     	String cdate = Date_current.main("d");
-            GDSEMR_frame.setTextAreaText(0, "공단검진 at GDS");
-            GDSEMR_frame.setTextAreaText(7, "\n  #  공단검진 at GDS  [ " + cdate + " ]");
+            GDSEMR_frame.setTextAreaText(0, "\t공단검진 at GDS clinic");
+            GDSEMR_frame.setTextAreaText(7, "\n  #  공단검진 at GDS clinic  [ " + cdate + " ]");
 
     }
 }
