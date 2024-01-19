ef1110e3df0f6509de6964f54c3bf76b466e1fcc
jae <jae@jae-virtual-machine>
2024-01-20 08:11:23 +0900

    234

--- src/je/panse/doro/fourgate/routinecheck/routinecheck.java
+++ src/je/panse/doro/fourgate/routinecheck/routinecheck.java
@@ -7,13 +7,13 @@ public class routinecheck {
     
     public static void GDSRC(String[] args) {
     	String cdate = Date_current.main("d");
-            GDSEMR_frame.setTextAreaText(0, "The patient was seen at the GDS clinic for a routine checkup on ." + cdate);
+            GDSEMR_frame.setTextAreaText(0, "The patient was seen at the GDS clinic \n\tfor a routine checkup on ." + cdate);
             GDSEMR_frame.setTextAreaText(7, "\n  #  GDS clinic Routine check  [ " + cdate + " ]");
 
     }
     public static void HCRC(String[] args) {
     	String cdate = Date_current.main("d");
-            GDSEMR_frame.setTextAreaText(0, "The patient was seen at the GDS clinic for a 공단검진 on " + cdate);
+            GDSEMR_frame.setTextAreaText(0, "The patient was seen at the GDS clinic \n\tfor a 공단검진 on " + cdate);
             GDSEMR_frame.setTextAreaText(7, "\n  #  공단검진 at GDS clinic  [ " + cdate + " ]");
 
     }
