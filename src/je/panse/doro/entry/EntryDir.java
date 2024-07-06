4ab5584a3885f34f620ea8eecda8526167defecc
dce040b <dce040b@dce040b-24.04>
2024-07-06 10:31:02 +0900

    123

--- src/je/panse/doro/entry/EntryDir.java
+++ src/je/panse/doro/entry/EntryDir.java
@@ -1,23 +1,17 @@
 package je.panse.doro.entry;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
+import java.net.URL;
 
 public class EntryDir {
-
-    public static String currentDir = System.getProperty("user.dir");
-
-    public static String homeDir = currentDir + "/src/je/panse/doro";
-//    public static String homeDir = currentDir + "/je/panse/doro";
-
-    public static String backupDir = homeDir + "/tripikata/rescue";
-
-    public static void main(String[] args) {
-        // Get the path to the current user's directory
-        System.out.println("Current user's directory: " + currentDir);
-        System.out.println("homeDir: " + homeDir);
+    public static void main(String[] args) throws Exception {
+        URL homeDirURL = EntryDir.class.getResource("/je/panse/doro");
+        if (homeDirURL != null) {
+            String homeDir = homeDirURL.getPath();
+            String backupDir = homeDir + "/tripikata/rescue";
+            System.out.println("homeDir: " + homeDir);
+            // Use homeDir and backupDir paths for read-only operations
+        } else {
+            System.out.println("Directory not found within JAR");
+        }
     }
 }
